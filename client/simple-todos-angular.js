e6ef6247fbf8bd2988006fe6d49aeff9104facf9
Chris Vaszauskas <chris.vaszauskas@gmail.com>
2015-10-03 13:06:10 -0500

    Complete tutorial step 3

--- client/simple-todos-angular.js
+++ client/simple-todos-angular.js
@@ -1,13 +1,9 @@
 angular.module('simple-todos', ['angular-meteor']);
 
-angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
+angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
 
-  function ($scope) {
-    $scope.tasks = [
-      { text: 'This is task 1' },
-      { text: 'This is task 2' },
-      { text: 'This is task 3' },
-    ];
+  function ($scope, $meteor) {
+    $scope.tasks = $meteor.collection(Tasks);
   },
 
 ]);
